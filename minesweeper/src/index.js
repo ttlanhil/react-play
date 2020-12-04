69ab77ee4139a3945b2b11b1dc21df015a97b3b1
Adam <ttlanhil@gmail.com>
2020-12-05 00:05:26 +1030

    Adding a timer and custom board sizes

--- minesweeper/src/index.js
+++ minesweeper/src/index.js
@@ -15,7 +15,7 @@ class Board extends React.Component {
         return (
             <Square
                 key={i}
-                // only show value once opened
+                // only show value once opened, otherwise show if it has a flag
                 value={this.props.squares[i].open ? this.props.squares[i].value : this.props.squares[i].flagged}
                 open={this.props.squares[i].open}
                 onClick={() => this.props.onClick(i)}
@@ -37,6 +37,69 @@ class Board extends React.Component {
                     </div>
                 )}
             </div>
+        );
+    }
+}
+
+class ElapsedTime extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            seconds: 0,
+        };
+    }
+
+    tick() {
+        this.setState(state => ({
+            seconds: state.seconds + 1
+        }));
+    }
+
+    componentDidUpdate() {
+        // respond to updates each tick (triggering state updates) or changes in props
+        if (this.props.stopped) {
+            this.stopTimer();
+        } else {
+            this.startTimer();
+        }
+    }
+
+    startTimer() {
+        if (! this.interval) {
+            this.interval = setInterval(() => this.tick(), 1000);
+            this.setState({seconds: 0});
+        }
+    }
+
+    stopTimer() {
+        if (this.interval) {
+            clearInterval(this.interval);
+            this.interval = null;
+        }
+    }
+
+    componentDidMount() {
+        this.startTimer();
+    }
+
+    componentWillUnmount() {
+        this.stopTimer();
+    }
+
+    formatTimeSince(elapsed) {
+        const hours = Math.floor(elapsed / 3600);
+        const minutes = Math.floor(elapsed / 60 % 60);
+        const seconds = Math.floor(elapsed % 60);
+
+        if (hours) {
+            return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        }
+        return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+    }
+
+    render() {
+        return (
+            <span>Time: {this.formatTimeSince(this.state.seconds)}</span>
         );
     }
 }
@@ -69,12 +132,25 @@ class Game extends React.Component {
     }
 
     setSize(width, height, count) {
+        // if one of the params is not specified, it's a custom board.
+        // Figure out current difficulty (how common a mine is), so that we can suggest the same rate for the new board
+        const oldDifficulty = this.state.count / (this.state.width * this.state.height);
+        while (! width) {
+            width = parseInt(prompt("Enter board width", this.state.width));
+        }
+        while (! height) {
+            height = parseInt(prompt("Enter board height", this.state.height));
+        }
+        while (! count) {
+            count = parseInt(prompt("Enter number of mines", Math.floor(width * height * oldDifficulty)));
+        }
+
         this.setState(this._resetState(width, height, count));
     }
 
     constructor(props) {
         super(props);
-        this.state = this._resetState(props.width || 10, props.height || 5, props.count || 5);
+        this.state = this._resetState(props.width || 10, props.height || 10, props.count || 10);
     }
 
     handleContextMenu(square, event) {
@@ -163,22 +239,36 @@ class Game extends React.Component {
         const resizes = Object.keys(presetSizes).map((name) => {
             const size = presetSizes[name];
             return (
-                <li key={size}>
-                    <button onClick={() => this.setSize(size.width, size.height, size.count)}>New {name} game</button>  ({size.width} x {size.height} with {size.count} mines)
-                </li>
+                <div key={name}>
+                    <button onClick={() => this.setSize(size.width, size.height, size.count)}>New {name} game</button>
+                    <br/>
+                    {size.width} x {size.height} with {size.count} mines
+                    <br/>
+                </div>
             );
-        });
+        }).concat(
+            <div key='custom'>
+                <button key='custom' onClick={() => this.setSize()}>New custom game</button>
+            </div>
+        );
 
-        let status = 'Waiting on player';
+        let status = 'Ready...';
         if (this.state.finished) {
             status = this.state.finished;
         }
+        status = <div>
+                    {status}<br/>
+                    Mines: {this.state.count}<br/>
+                    Flags: {this.state.squares.filter(s => s.flagged).length}<br/>
+                    Hidden: {this.state.squares.filter(s => ! s.open).length}<br/>
+                    <ElapsedTime stopped={this.state.finished} />
+                </div>
 
         return (
             <div className="game">
                 <div className="game-info">
                     <div>{status}</div>
-                    <ul>{resizes}</ul>
+                    {resizes}
                 </div>
                 <div className="game-board">
                 <Board
